b70219a06ff1b9914e7bb3f77dc4f1ff5cda0220
madigraf <grafmadison@gmail.com>
2018-05-11 20:18:21 -0700

    added relevant functionality from html file to Lightbug class

--- src/lightbug.js
+++ src/lightbug.js
@@ -1,12 +1,16 @@
 class Lightbug{
     constructor(game, cWith){
+
+
         //needs param:
         //  game
 
         //needs to create/receive:
         //  group
         //  collisionGroup
-        //  who it collides with
+        //  who it collides with and their callbacks - an array of arrays
+        //formatted like [[collision group, callback]....]
+        //normally the collides function can take more than one element with a callback, but this makes my life easier
 
         this.game = game;
 
@@ -26,7 +30,22 @@ class Lightbug{
         this.cWith = cWith;
     }
 
+    static hitPellet(playerBody, pelletBody){
+        //for callback, first param is the one being collided with, second is the collider
+        if(!pelletBody.hasCollided){
+            pelletBody.hasCollided = true;
+            //!!! should be replaced with a more reliable method of assuring pelletHit only increments once in the future
+            //i haven't had any issues with this but technically it is still not async safe (consider incrementing when
+            //things are going to be destroyed)
+            pelletBody.sprite.kill();
+            pelletHit++;
+            text.text = "pellets hit: " + pelletHit;
+        }
+        
+    }
+
     createLightbug(x, y){
+        var i;
         let lb = this.group.create(x, y, 'firefly');
         lb.anchor.setTo(.5);
 
@@ -36,7 +55,11 @@ class Lightbug{
         game.debug.body(lb);    
         
         lb.body.setCollisionGroup(this.cGroup);
-        lb.body.collides(this.cWith);
+        for(i=0; i < this.cWith.length; i++){
+            lb.body.collides(this.cWith[i][0], this.cWith[i][1]);
+        }
+       
+        return lb;
     }
 
 
