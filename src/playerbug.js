e595b19a7099b7cebbf57b178ad69f192d46106c
madigraf <grafmadison@gmail.com>
2018-05-11 22:40:12 -0700

    moved relevant functionality to Playerbug class

--- src/playerbug.js
+++ src/playerbug.js
@@ -1,6 +1,64 @@
 class Playerbug{
-    constructor(){
+    constructor(game, player){
+        //to be done at a later date
+        this.game = game;
+        this.player = player;
+    }
+
+    move(){
+        var angleChange = 5; //degrees for which the player will turn at per update
+        //do not change angle if cursor is in body
+        var point = new Phaser.Point(this.game.input.worldX, this.game.input.worldY);
+        var bodies = this.game.physics.p2.hitTest(point, [this.player.body]);
+        //console.log(bodies.length);
+        if(!bodies.length){
+            var cursorAngle =  this.game.math.radToDeg(this.game.math.angleBetween(
+            this.player.body.x, this.player.body.y,
+            this.game.input.worldX, this.game.input.worldY)) + 90;
+            
+            if(cursorAngle < 0){
+                cursorAngle += 360;
+            }
+
+            var bodyAngle = this.player.body.angle;
+
+            if(bodyAngle < 0){
+                bodyAngle += 360;
+            }
+
+            //determine the angle to rotate
+            var difference = cursorAngle - bodyAngle;
+            if(difference < -180){
+                difference += 360;
+            } 
+            if (difference > 180){
+                difference -= 360;
+            } 
+
+            //apply the appropriate rotation
+            if(difference < 0){
+                if((difference * -1) > angleChange){
+                    this.player.body.angle -= angleChange;
+                }
+                else{
+                    this.player.body.angle += difference;
+                }
+            }
+            else if(cursorAngle > 0){
+                if(difference  > angleChange){
+                    this.player.body.angle += angleChange;
+                }
+                else{
+                    this.player.body.angle += difference;
+                }
+            }        
+            
+        }
+        this.player.body.moveForward(200);
         
+
+        //player.body.x = game.input.worldX;
+        //player.body.y = game.input.worldY;
     }
 
 }
